988bb38450bc7b96ab2b0039af26eb00db6946ca
Deenie-Wallace <jubilant.deenie@gmail.com>
2019-06-17 12:31:05 -0700

    integrated router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import uuid from 'uuid';
 
 
@@ -62,10 +63,15 @@ class App extends Component {
       <div className="App">
         <div className="container">
           <Header />
-          <AddTodo addTodo={this.addTodo}/>
-          <Todos todos={this.state.todos}
-          markComplete={this.markComplete}
-          delTodo={this.delTodo} />
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo}/>
+                  <Todos todos={this.state.todos}
+                  markComplete={this.markComplete}
+                  delTodo={this.delTodo} />
+            </React.Fragment>
+          )} />
+          <Route path="/about" component={About} />
         </div>
       </div>
       </Router>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <React.Fragment>
             <h1>About</h1>
-            <p>This is the TodoLIst app v1.0. 
+            <p>This is the TodoList app v1.0. 
             This is part of a React crash course.</p>
         </React.Fragment>
     )
