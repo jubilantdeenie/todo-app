8d2baf928cf5430d6d42749164c0f181f71fe5d1
Deenie-Wallace <jubilant.deenie@gmail.com>
2019-06-18 09:47:15 -0700

    deploy on github pages test

--- src/App.js
+++ src/App.js
@@ -34,8 +34,7 @@ class App extends Component {
   // Delete Todo
   delTodo = (id) => {
     axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(res => this.setState({ todos: [...this.state.todos.filter
-        (todo => todo.id !== id)] }));
+      .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }));
   }
   
  // Add Todo
