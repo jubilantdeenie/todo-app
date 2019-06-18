3550ada1fe262d8b0303419486be24b8bd537111
Deenie-Wallace <jubilant.deenie@gmail.com>
2019-06-18 10:48:11 -0700

    Updates

---
+++ precache-manifest.5cc68a4b0fe2558d68013255b5d5ebca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7639c570b41d9f1ae299314c0f621915",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "930e38cb4d0fb5caa9ff",
+    "url": "/todo-app/static/css/main.38eb4694.chunk.css"
+  },
+  {
+    "revision": "2edf744ede4470d76a5c",
+    "url": "/todo-app/static/js/2.39cfb255.chunk.js"
+  },
+  {
+    "revision": "930e38cb4d0fb5caa9ff",
+    "url": "/todo-app/static/js/main.624583b8.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
